55b1c0f39f52df8a6d7141ec2c335c5f55c255cb
PipaPipe <74451005+PipaPipe@users.noreply.github.com>
2021-06-29 22:55:33 +0500

    Бот

---
+++ Daily planner/Class_task.py
@@ -0,0 +1,24 @@
+
+
+class Task():
+
+    def __init__(self ):
+        self.task_list = [1, 2, 3]
+
+
+    def add_task(self):
+        pass
+
+
+    def delete_task(self):
+        pass
+
+
+    def edit_task(self):
+        pass
+
+
+    def viewall_task(self):
+        pass
+
+# Создать класс на основе класса list, при помощи библиотеки time
